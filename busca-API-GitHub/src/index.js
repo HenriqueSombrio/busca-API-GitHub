a2fe575dc9b3de0c32baa7adb2d0a2efdf4fe1c7
Henrique <henriquesombrio3@gmail.com>
2020-06-10 10:52:09 -0300

    Ajustes mobile

---
+++ busca-API-GitHub/src/index.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    if (document.getElementById("demo").style.display == "block") {
+        document.getElementById("demo").style.display = "none";
+    } else {
+        document.getElementById("demo").style.display = "block";
+    }
+} 
